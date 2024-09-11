8ca07e831d2dc4f01813fea9e9adc03e4ead943f
Santiago Verdu <erra@qmake.org>
2024-09-10 19:12:04 -0600

    port

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
 import { constructApp } from "./app";
 
-let port: any;
 const app = constructApp();
 
-if (process.env.APP_ENV === "development") {
-  port = 8081;
-} else {
-  port = process.env.PORT || 3000;
-}
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.listen(8081, () => {
+  console.log(`App listening on port 8081`);
 });
