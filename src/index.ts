633567579a3d64e47e1691a01d1410823b9ee8cb
Santiago Verdu <erra@qmake.org>
2024-09-10 19:32:04 -0600

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { constructApp } from "./app";
+import cors from "cors";
 
 const app = constructApp();
+
+app.use(cors());
 
 app.listen(8081, () => {
   console.log(`App listening on port 8081`);
